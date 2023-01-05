b93d1952635a4947a84a95d7f9b86dd1380b8fbd
dima-yarakhovich <dima020490@tut.by>
2023-01-05 15:14:20 +0300

    Done hw28_Библиотеки

--- src/main/java/com/skypro/recipe/controller/IngridientController.java
+++ src/main/java/com/skypro/recipe/controller/IngridientController.java
@@ -1,6 +1,17 @@
 package com.skypro.recipe.controller;
+
 import com.skypro.recipe.model.Ingridient;
+import com.skypro.recipe.model.Recipte;
 import com.skypro.recipe.service.impl.IngridientServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -9,6 +20,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/ingridient")
+@Tag(name = "Ингредиенты", description = "Операции добавления/удаления/редактирования/просмотра ингредиентов:")
 public class IngridientController {
     private final IngridientServiceImpl ingridientService;
 
@@ -17,11 +29,41 @@ public class IngridientController {
     }
 
     @PostMapping
+    @Operation(summary = "Добавление ингридиента")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент добавлен",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingridient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<?> addIngridient(@RequestBody Ingridient ingridient) {
         return ResponseEntity.ok(ingridientService.addIngridient(ingridient));
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Просмотр ингридиента", description = "можно искать по порядковому номеру")
+    @Parameters(value = {
+            @Parameter(example = "0-100")
+
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Просмотр ингредиента",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingridient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity getIngridient(@PathVariable Long id) {
         Ingridient ingridient = this.ingridientService.getIngridient(id);
         if (ingridient == null) {
@@ -29,19 +71,60 @@ public class IngridientController {
         }
         return ResponseEntity.ok(ingridient);
     }
+
     @GetMapping
+    @Operation(summary = "Просмотр всех ингридиентов приложения")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Список ингредиентов:",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingridient.class))
+                            )
+                    }
+            )
+    })
     public Collection<Ingridient> getAllIngridient() {
         return this.ingridientService.getAllIngridient();
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary = "Удаление ингридиента", description = "нужно удалять по порядковому номеру")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент удален",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingridient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Void> deleteIngridient(@PathVariable long id) {
         if (ingridientService.deleteIngridient(id)) {
             return ResponseEntity.ok().build();
         }
         return ResponseEntity.notFound().build();
     }
+
     @PutMapping("/{id}")
+    @Operation(summary = "Редактирование ингридиента", description = "можно редактировать по порядковому номеру")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Ингредиент отредоктирован",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation = Ingridient.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<Ingridient> editIngridient(@PathVariable long id, @RequestBody Ingridient ingridient) {
         Ingridient ingridient1 = ingridientService.editIngridient(id, ingridient);
         if (ingridient == null) {
--- src/main/java/com/skypro/recipe/controller/RecipteController.java
+++ src/main/java/com/skypro/recipe/controller/RecipteController.java
@@ -2,6 +2,15 @@ package com.skypro.recipe.controller;
 
 import com.skypro.recipe.model.Recipte;
 import com.skypro.recipe.service.impl.RecipteServiceImpl;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.Parameters;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,6 +19,7 @@ import java.util.Collection;
 
 @RestController
 @RequestMapping("/recipte")
+@Tag(name = "Рецепты",description = "Операции добавления/удаления/редактирования/просмотра рецептов:")
 public class RecipteController {
     private final RecipteServiceImpl recipteService;
 
@@ -18,23 +28,79 @@ public class RecipteController {
     }
 
     @GetMapping("/{id}")
+    @Operation(summary = "Просмотр рецепта",description = "можно искать по порядковому номеру")
+    @Parameters(value = {
+            @Parameter(example = "0-100")
+
+    })
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт найден",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation =Recipte.class))
+                            )
+                    }
+            )
+    })
     public Recipte getRecipe(@PathVariable Long id) {
         return this.recipteService.getRecipte(id);
     }
 
 
     @PostMapping
+    @Operation(summary = "Добавление рецепта")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт добавлен",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation =Recipte.class))
+                            )
+                    }
+            )
+    })
     public ResponseEntity<?> addRecipe(@RequestBody Recipte recipte) {
         return ResponseEntity.ok(recipteService.addRecipte(recipte));
     }
 
     @GetMapping
+    @Operation(summary = "Просмотр всех рецептов приложения")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Все рецерты доступны для просмотра",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation =Recipte.class))
+                            )
+                    }
+            )
+    })
     public Collection<Recipte> getAllRecipte() {
         return this.recipteService.getAllRecipte();
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<Void> deleteRecipte(@PathVariable long id) {
+    @Operation(summary = "Удаление рецепта",description = "нужно удалять по порядковому номеру")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт удален",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation =Recipte.class))
+                            )
+                    }
+            )
+    })
+    public  ResponseEntity<Void> deleteRecipte(@PathVariable long id) {
         if (recipteService.deleteRecipte(id)) {
             return ResponseEntity.ok().build();
         }
@@ -42,6 +108,19 @@ public class RecipteController {
     }
 
     @PutMapping("/{id}")
+    @ApiResponses(value = {
+            @ApiResponse(
+                    responseCode = "200",
+                    description = "Рецепт отредактирован",
+                    content = {
+                            @Content(
+                                    mediaType = "aplication/json",
+                                    array = @ArraySchema(schema = @Schema(implementation =Recipte.class))
+                            )
+                    }
+            )
+    })
+    @Operation(summary = "Редактирование рецепта",description = "можно редактировать по порядковому номеру")
     public ResponseEntity<Recipte> editRecipte(@PathVariable long id, @RequestBody Recipte recipte) {
         Recipte recipte1 = recipteService.editRecipte(id, recipte);
         if (recipte == null) {
